2debcbc63e2a280a32b83b6ad2afc7d0bd5fb834
liuche <706716852@qq.com>
2023-09-03 18:33:43 +0800

    1-2 添加了全局使用组件以及图标

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ADropdownButton/>
+    <a-button type="primary" danger ghost>Danger</a-button>
+    <step-backward-outlined />
   </div>
 </template>
