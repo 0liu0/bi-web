fac2359a307e61091a84773df196f8f0430fbe3a
liuche <706716852@qq.com>
2023-09-09 01:19:27 +0800

    2-4 主要调试了上传组件，对其他参数做了小的更改。

--- src/components/content/item/GenChart.vue
+++ src/components/content/item/GenChart.vue
@@ -1,36 +1,41 @@
 <template>
-  <div :style="bgStyle">
-    <h1>这是生成图表内容</h1>
-    <a-row>
-      <a-col :span="8" style="">
-        <h1>智能分析</h1>
-        <br>
-        <a-form :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol">
-          <a-form-item label="表名">
-            <a-input v-model:value="formState.name" />
-          </a-form-item>
-          <a-form-item label="分析目标">
-            <a-textarea v-model:value="formState.goal" />
-          </a-form-item>
-          <a-form-item label="图表类型" name="region">
-            <a-select v-model:value="formState.chartType" placeholder="选择您想要生成的图表类型">
-              <a-select-option value="line">线型图</a-select-option>
-              <a-select-option value="bar">柱状图</a-select-option>
-            </a-select>
-          </a-form-item>
-          <!--上传文件组件-->
-          <a-form-item label="上传文件">
-            <a-upload :beforeUpload="beforeUpload">
-              <a-button style="margin-left: -242px">
-                <a-icon type="upload" /> 点击上传
-              </a-button>
-            </a-upload>
-          </a-form-item>
-          <a-form-item style="margin-left: -60px" :wrapper-col="{ span: 14, offset: 4 }">
-            <a-button type="primary" @click="onSubmit">Create</a-button>
-            <a-button style="margin-left: 10px">Cancel</a-button>
-          </a-form-item>
-        </a-form>
+  <div :style="bgStyle" style="height: 1120px">
+    <h1 style="font-size: 50px">这是生成图表内容</h1>
+    <a-row style="margin-top: 60px">
+      <a-col :span="8">
+        <div class="upload">
+          <h1>智能分析</h1>
+          <br>
+          <a-form :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol">
+            <a-form-item label="表名">
+              <a-input v-model:value="formState.name" />
+            </a-form-item>
+            <br>
+            <a-form-item label="分析目标">
+              <a-textarea v-model:value="formState.goal" />
+            </a-form-item>
+            <br>
+            <a-form-item label="图表类型" name="region">
+              <a-select v-model:value="formState.chartType" placeholder="选择您想要生成的图表类型">
+                <a-select-option value="line">线型图</a-select-option>
+                <a-select-option value="bar">柱状图</a-select-option>
+              </a-select>
+            </a-form-item>
+            <br>
+            <!--上传文件组件-->
+            <a-form-item label="上传文件">
+              <a-upload :beforeUpload="beforeUpload">
+                <a-button style="margin-left: -186px">
+                  <CloudUploadOutlined/> 上传CSV文件
+                </a-button>
+              </a-upload>
+            </a-form-item>
+
+            <a-form-item style="margin-left: -135px" :wrapper-col="{ span: 14, offset: 4 }">
+              <a-button type="primary" @click="onSubmit">提交</a-button>
+            </a-form-item>
+          </a-form>
+        </div>
       </a-col>
       <a-col :span="16" style="background-color:#333;">
         <h1>分析结论</h1>
@@ -79,6 +84,7 @@ const onSubmit = () => {
   }).then(resp => {
     if (resp.data.code===0) {
       message.success("上传成功！正在解析中…………")
+      // todo 将信息分给右侧去生成对应的信息
     }else {
       message.error(resp.data.msg)
     }
@@ -91,4 +97,14 @@ const beforeUpload = (file) => {
 </script>
 
 <style scoped>
+.upload {
+  width: 600px;
+  height: 500px;
+  margin-left: 100px;
+  border-color: #3399ff;
+  border-radius: 20px;
+  padding: 25px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  background-color: rgba(255,255,255,.75);
+}
 </style>
